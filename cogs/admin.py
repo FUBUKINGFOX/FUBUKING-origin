c28dca72a8883d5bc487fbca9a762928c9ae986d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-10 01:16:30 +0800

    update

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
 
-class admin(commands.cog):
+class admin(commands.Cog):
     def __init__(self, bot) :
         self.bot = bot
 
     @commands.command()
     @commands.is_owner()
     async def cmd(self, ctx, int ):
-        if int == 1
+        if int == 1 :
+            pass
 
 
 def setup(bot):
